b5e5aaf8c7cbd73e2ce01559156cfcf5cb8af496
krzm <kmazanek@gmail.com>
2022-05-12 19:10:38 +0200

    add loop

---
+++ Sim.Core/IGameLoop.cs
@@ -0,0 +1,16 @@
+﻿namespace Sim.Core;
+
+public interface IGameLoop
+{
+    event Action<GameStats> SetLabelEvent;
+
+    void Log();
+
+    void ProcessInput();
+
+    void Render();
+
+    void RunGameLoop();
+
+    void Update(double ms);
+}
