e91949d8a03e00d5f5b682913203b5289d8ffc2b
krzm <kmazanek@gmail.com>
2022-05-18 22:18:42 +0200

    Add xml config tool

---
+++ Sim.Core/Core/ConfigProvider.cs
@@ -0,0 +1,74 @@
+﻿using System.Configuration;
+
+namespace Sim.Core;
+
+public class ConfigProvider
+    : IConfigProvider
+{
+    public void ReadAllSettings()
+    {
+        try
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+
+            if (appSettings.Count == 0)
+            {
+                Console.WriteLine("AppSettings is empty.");
+            }
+            else
+            {
+                foreach (var key in appSettings.AllKeys)
+                {
+                    Console.WriteLine("Key: {0} Value: {1}", key, appSettings[key]);
+                }
+            }
+        }
+        catch (ConfigurationErrorsException)
+        {
+            Console.WriteLine("Error reading app settings");
+        }
+    }
+
+    public string ReadSetting(string key)
+    {
+        try
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            var result = appSettings[key] ?? "Not Found";
+            Console.WriteLine(result);
+            return result;
+        }
+        catch (ConfigurationErrorsException)
+        {
+            Console.WriteLine("Error reading app settings");
+            return string.Empty;
+        }
+        catch (Exception)
+        {
+            return string.Empty;
+        }
+    }
+
+    public void AddUpdateAppSettings(string key, string value)
+    {
+        try
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            if (settings[key] == null)
+            {
+                settings.Add(key, value);
+            }
+            else
+            {
+                settings[key].Value = value;
+            }
+            configFile.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+        }
+        catch (ConfigurationErrorsException)
+        {
+            Console.WriteLine("Error writing app settings");
+        }
+    }
+}
---
+++ Sim.Core/Core/IConfigProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Sim.Core;
+
+public interface IConfigProvider
+{
+    void AddUpdateAppSettings(string key, string value);
+
+    void ReadAllSettings();
+
+    string ReadSetting(string key);
+}
